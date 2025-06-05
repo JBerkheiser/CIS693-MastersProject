d37a8dc1802634696362b8629ba50bf15c8a9122
JBerkheiser <berkheij@mail.gvsu.edu>
2025-06-05 16:00:10 -0400

    wake word works in venv, calls combined test prints vocal prompt on screen

--- source/frontend/CombinedTest.py
+++ source/frontend/CombinedTest.py
@@ -1,14 +1,13 @@
-from gpiozero import Button
+#!/usr/bin/env python3
+
 from datetime import datetime
 import os
 import requests
 
 url = "https://berkheiser-cis693.uk.r.appspot.com/prompt"
 
-button = Button(27)
-button.wait_for_press()
 date = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-print("The button was pressed, recording")
+
 os.system(f'arecord -D hw:0,0 --format S16_LE --duration=5 --rate 48000 -c2 /home/joseberk/CIS693-MastersProject/testSounds/{date}.wav')
 
 files = {"prompt": open(f'/home/joseberk/CIS693-MastersProject/testSounds/{date}.wav', 'rb')}
---
+++ source/frontend/WakeWordTest.py
@@ -0,0 +1,33 @@
+import pvporcupine
+import subprocess
+import numpy as np
+import time
+
+
+def get_next_audio_frame():
+	rawBytes = process.stdout.read(frameLength * bytesPerSample * channels)
+	samples = np.frombuffer(rawBytes, dtype=np.int16)
+	rightChannel = samples[1::2]
+	return rightChannel
+
+keyWordPath = "/home/joseberk/CIS693-MastersProject/includes/porcupine/Hey-Torkelson_en_raspberry-pi_v3_0_0.ppn"
+file = open("/home/joseberk/CIS693-MastersProject/includes/porcupine/PorcupineAccessKey.txt", "r")
+accessKey = file.read()
+file.close()
+
+bytesPerSample = 2
+channels = 2
+
+while True:
+	handle = pvporcupine.create(access_key=accessKey, keyword_paths=[keyWordPath])
+	frameLength = handle.frame_length
+	process = subprocess.Popen(['arecord', '-D',  'hw:0,0', '-f', 'S16_LE', '-r', '16000', '-c', '2', '-t', 'raw'], stdout=subprocess.PIPE)
+	while True:
+		keywordIndex = handle.process(get_next_audio_frame())
+		if keywordIndex >= 0:
+			print("Heard the word")
+			break
+	time.sleep(1)
+	process.kill()
+	subprocess.call("/home/joseberk/CIS693-MastersProject/source/frontend/CombinedTest.py")
+	handle.delete()
