96426e2389481ed01cca2c3c4ff8c54f0f4d152e
Joe Berkheiser <berkheij@mail.gvsu.edu>
2025-06-19 14:16:02 -0400

    added a new endpoint in the backend to analyze photos

--- source/backend/main.py
+++ source/backend/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google import genai
 from google.cloud import speech
+from google.genai import types
 from google.genai.types import (
     GenerateContentConfig,
     GoogleSearch,
@@ -48,6 +49,38 @@ def Prompt():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=question,
+        config=GenerateContentConfig(
+            tools=[
+                Tool(google_search=GoogleSearch())
+            ],
+        ),
+    )
+
+    print(f"Response: {response.text}")
+    return jsonify({"Response": response.text})
+
+@app.route("/photo", methods=["POST"])
+def Photo():
+    rawData = ""
+    if "rawData" not in request.files:
+        return jsonify({"error": "No image file sent"}), 400
+    imageBytes = request.files["rawData"]
+    if imageBytes == "":
+        return jsonify({"error": "No image file sent"}), 400
+    
+    question = f"Describe the following image in less than five sentences."
+    print(f"Question: {question}")
+    client = genai.Client(vertexai=True, project="berkheiser-cis693", location="us-central1")
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=[
+            types.Part.from_bytes(
+                data=imageBytes,
+                mime_type='image/jpeg',
+            ),
+            question
+        ],
         config=GenerateContentConfig(
             tools=[
                 Tool(google_search=GoogleSearch())
